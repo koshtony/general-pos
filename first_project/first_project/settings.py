2dbba5ce8a04817b019d71102968aea0d02bcfe8
kosh <kosh@localhost.localdomain>
2023-07-20 21:44:37 +0300

    solved 500 server err

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -153,7 +153,7 @@ CSRF_TRUSTED_ORIGINS = ['https://general-pos-production.up.railway.app','https:/
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATIC_ROOT =  os.path.join(BASE_DIR,'firstapp/static')
 
